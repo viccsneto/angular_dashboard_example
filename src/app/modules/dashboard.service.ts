01079214ffc377b4eda62b553cc04eabf22ce885
Vicente Neto <vic.cs.neto@gmail.com>
2021-01-07 23:59:37 -0300

    Adds more echarts examples

--- src/app/modules/dashboard.service.ts
+++ src/app/modules/dashboard.service.ts
@@ -4,28 +4,369 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DashboardService {
+  NGXECharts1() {
+    const xAxisData = [];
+    const data1 = [];
+    const data2 = [];
+
+    for (let i = 0; i < 100; i++) {
+      xAxisData.push('category' + i);
+      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    }
+
+    let options = {
+      legend: {
+        data: ['bar', 'bar2'],
+        align: 'left',
+      },
+      tooltip: {},
+      xAxis: {
+        data: xAxisData,
+        silent: false,
+        splitLine: {
+          show: false,
+        },
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'bar',
+          type: 'bar',
+          data: data1,
+          animationDelay: (idx) => idx * 10,
+        },
+        {
+          name: 'bar2',
+          type: 'bar',
+          data: data2,
+          animationDelay: (idx) => idx * 10 + 100,
+        },
+      ],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate: (idx) => idx * 5,
+    };
+
+    return options;
+  }
+
+  NGXECharts2() {
+    let options = {
+      title: {
+        text: '堆叠区域图',
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985',
+          },
+        },
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎'],
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+        },
+      ],
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+            },
+          },
+          areaStyle: {},
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+    };
+
+    return options;
+  }
+
+  NGXECharts3() {
+    let options = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          crossStyle: {
+            color: '#999',
+          },
+        },
+      },
+      toolbox: {
+        feature: {
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ['line', 'bar'] },
+          restore: { show: true },
+          saveAsImage: { show: true },
+        },
+      },
+      legend: {
+        data: ['蒸发量', '降水量', '平均温度'],
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: [
+            '1月',
+            '2月',
+            '3月',
+            '4月',
+            '5月',
+            '6月',
+            '7月',
+            '8月',
+            '9月',
+            '10月',
+            '11月',
+            '12月',
+          ],
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: '水量',
+          min: 0,
+          max: 250,
+          interval: 50,
+          axisLabel: {
+            formatter: '{value} ml',
+          },
+        },
+        {
+          type: 'value',
+          name: '温度',
+          min: 0,
+          max: 25,
+          interval: 5,
+          axisLabel: {
+            formatter: '{value} °C',
+          },
+        },
+      ],
+      series: [
+        {
+          name: '蒸发量',
+          type: 'bar',
+          data: [
+            2.0,
+            4.9,
+            7.0,
+            23.2,
+            25.6,
+            76.7,
+            135.6,
+            162.2,
+            32.6,
+            20.0,
+            6.4,
+            3.3,
+          ],
+        },
+        {
+          name: '降水量',
+          type: 'bar',
+          data: [
+            2.6,
+            5.9,
+            9.0,
+            26.4,
+            28.7,
+            70.7,
+            175.6,
+            182.2,
+            48.7,
+            18.8,
+            6.0,
+            2.3,
+          ],
+        },
+        {
+          name: '平均温度',
+          type: 'line',
+          yAxisIndex: 1,
+          data: [
+            2.0,
+            2.2,
+            3.3,
+            4.5,
+            6.3,
+            10.2,
+            20.3,
+            23.4,
+            23.0,
+            16.5,
+            12.0,
+            6.2,
+          ],
+        },
+      ],
+    };
+
+    return options;
+  }
+
+  NGXECharts4() {
+    function genData(len, offset) {
+      var lngRange = [-10.781327, 131.48];
+      var latRange = [18.252847, 52.33];
+
+      var arr = new Float32Array(len * 2);
+      var off = 0;
+
+      for (var i = 0; i < len; i++) {
+        var x = +Math.random() * 10;
+        var y =
+          +Math.sin(x) -
+          x * (len % 2 ? 0.1 : -0.1) * Math.random() +
+          (offset || 0) / 10;
+        arr[off++] = x;
+        arr[off++] = y;
+      }
+      return arr;
+    }
+
+    var data1 = genData(5e5, 0);
+    var data2 = genData(5e5, 10);
+    let options = {
+      title: {
+        text:
+          (data1.length / 2 + data2.length / 2) +
+          ' Points',
+      },
+      tooltip: {},
+      toolbox: {
+        left: 'center',
+        feature: {
+          dataZoom: {},
+        },
+      },
+      legend: {
+        orient: 'vertical',
+        right: 10,
+      },
+      xAxis: [{}],
+      yAxis: [{}],
+      dataZoom: [
+        {
+          type: 'inside',
+        },
+        {
+          type: 'slider',
+        },
+      ],
+      animation: false,
+      series: [
+        {
+          name: 'A',
+          type: 'scatter',
+          data: data1,
+          dimensions: ['x', 'y'],
+          symbolSize: 3,
+          itemStyle: {
+            opacity: 0.4,
+          },
+          large: true,
+        },
+        {
+          name: 'B',
+          type: 'scatter',
+          data: data2,
+          dimensions: ['x', 'y'],
+          symbolSize: 3,
+          itemStyle: {
+            opacity: 0.4,
+          },
+          large: true,
+        },
+      ],
+    };
+
+    return options;
+  }
+
   Table() {
     return [
-      {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-      {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-      {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-      {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-      {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-      {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-      {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-      {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-      {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-      {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-      {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-      {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-      {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-      {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-      {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-      {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-      {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-      {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-      {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-      {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+      { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+      { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+      { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+      { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+      { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+      { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+      { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+      { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+      { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+      { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+      { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+      { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+      { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+      { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+      { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
     ];
   }
   Cards() {
--- src/app/modules/posts/posts.component.ts
+++ src/app/modules/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './../dashboard.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,50 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  options: any;
-  constructor() {}
+  all_options = [];
+  constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {
-    const xAxisData = [];
-    const data1 = [];
-    const data2 = [];
-
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
-
-    this.options = {
-      legend: {
-        data: ['bar', 'bar2'],
-        align: 'left',
-      },
-      tooltip: {},
-      xAxis: {
-        data: xAxisData,
-        silent: false,
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {},
-      series: [
-        {
-          name: 'bar',
-          type: 'bar',
-          data: data1,
-          animationDelay: (idx) => idx * 10,
-        },
-        {
-          name: 'bar2',
-          type: 'bar',
-          data: data2,
-          animationDelay: (idx) => idx * 10 + 100,
-        },
-      ],
-      animationEasing: 'elasticOut',
-      animationDelayUpdate: (idx) => idx * 5,
-    };
+    this.all_options.push(this.dashboardService.NGXECharts1());
+    //this.all_options.push(this.dashboardService.NGXECharts2());
+    //this.all_options.push(this.dashboardService.NGXECharts3());
+    //this.all_options.push(this.dashboardService.NGXECharts4());
   }
 }
