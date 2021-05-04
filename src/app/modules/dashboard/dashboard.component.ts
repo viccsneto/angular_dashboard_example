82cf230d572913e6434e3bb18711a7425ea49d47
Vicente Correa da Silva Neto <vic.cs.neto@gmail.com>
2021-05-04 11:50:52 -0300

    Improves demo scene a little bit

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThreejscanvasComponent } from 'src/app/shared/widgets/threejscanvas/threejscanvas.component';
 import { DashboardService } from '../dashboard.service';
 
 @Component({
@@ -12,11 +13,7 @@ export class DashboardComponent implements OnInit {
   cards = [];
   pie = [];
   table = [];
-  mesh: any;
-  mesh2: any;
-  geometry: any;
-  material: any;
-  material2: any;
+
   constructor(
     private dashboardService: DashboardService
   ) {
@@ -32,30 +29,47 @@ export class DashboardComponent implements OnInit {
 
   initialize3DCanvas(THREE)
   {
-    //console.log("initialize3DCanvas", THREE);
-    THREE.geometry = new  THREE.IcosahedronGeometry( 200, 1 )
-    THREE.material = new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
-    THREE.material2 = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
-    THREE.mesh1 = new THREE.Mesh( THREE.geometry, THREE.material );
-    THREE.mesh2 = new THREE.Mesh( THREE.geometry, THREE.material2 );
-    THREE.mesh2.scale.x = 0.5;
-    THREE.mesh2.scale.y = 0.5;
-    THREE.mesh2.scale.z = 0.5;
+    THREE.geoSphere = new  THREE.SphereGeometry( 200, 8, 8)
 
-    THREE.scene.add( THREE.mesh1 );
-    THREE.scene.add( THREE.mesh2 );
+    THREE.matRed = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    THREE.mshRedSphere = new THREE.Mesh( THREE.geoSphere, THREE.matRed );
+    THREE.scene.add( THREE.mshRedSphere );
 
+    THREE.matYellow = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
+    THREE.mshYellowSphere = new THREE.Mesh( THREE.geoSphere, THREE.matYellow );
+    THREE.mshYellowSphere.scale.set(0.5, 0.5, 0.5);
+    THREE.scene.add( THREE.mshYellowSphere );
+
+    THREE.matWhite = new THREE.MeshBasicMaterial( { color: 0xffffff});
+    THREE.mshWhiteSphere = new THREE.Mesh( THREE.geoSphere, THREE.matWhite );
+    THREE.mshWhiteSphere.position.set( 500, 0, 0 );
+    THREE.mshWhiteSphere.scale.x = 0.15;
+    THREE.mshWhiteSphere.scale.y = 0.15;
+    THREE.mshWhiteSphere.scale.z = 0.15;
+
+    THREE.light1 = new THREE.PointLight( 0xffffff, 0.25, 700 )
+    THREE.mshWhiteSphere.position.y = -900;
+    THREE.mshWhiteSphere.add(THREE.light1);
+
+    THREE.mshYellowSphere.add(THREE.mshWhiteSphere);
+
+    THREE.matFloor = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0.5, metalness: 1 } );
+    THREE.geoFloor = new THREE.BoxGeometry( 2000, 80, 2000 );
+		THREE.mshFloor = new THREE.Mesh( THREE.geoFloor, THREE.matFloor );
+    THREE.mshFloor.position.y -= 550;
+		THREE.scene.add( THREE.mshFloor );
+
+    THREE.renderer.setClearColor(0x0f0f3f);
 
     THREE.controls = new THREE.CameraControls( THREE.camera, THREE.renderer.domElement );
-    THREE.controls.target.copy( THREE.mesh2.position );
+    THREE.controls.target.copy( THREE.mshYellowSphere.position );
 		THREE.controls.update()
-
   }
 
   update3DCanvas(THREE)
   {
-    THREE.mesh1.rotation.y -= 0.2 * THREE.elapsedTime;
-    THREE.mesh2.rotation.y += 0.5 * THREE.elapsedTime;
+    THREE.mshRedSphere.rotation.y -= 0.2 * THREE.elapsedTime;
+    THREE.mshYellowSphere.rotation.y += 0.5 * THREE.elapsedTime;
   }
 
 }
